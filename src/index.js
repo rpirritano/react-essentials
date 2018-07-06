4266f56258a9fdfeade3f436f66363133b11527d
Robert Pirritano <rpirritano@gmail.com>
2018-07-06 08:45:52 -1000

    changed to be built with webpack

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ render(
 	<h1 id='title'
 			className = 'header'
 			style = {{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
-			Hello World!
+			Hello World: BADDOBEE!
 
 	</h1>,
 	document.getElementById('react-container')
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+//describes everything we want webpack to do to ready them for production
+
+//import webpack
+var webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: "dist/assets",
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	devServer: {  //similar to httpster, but refresh reloads - hot reloading
+		inline: true,
+		contentBase: './dist',
+		port: 3000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/, //where to search - all files with.js
+				exclude: /(node_modules)/, //dont look in these files
+				loader: ["babel-loader"],
+				query: {
+					presets: ["latest", "stage-0", "react"]
+				}
+			}
+		]
+	}
+}
